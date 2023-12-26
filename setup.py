c4abc271a305e80409ca75f8142043ed2083e97e
Kristina Gagalova <kristina.gagalova@gmail.com>
2023-12-26 21:15:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,12 +71,12 @@ setup(
 
     install_requires=[
         "python-baseconv",
-        "gffpal=v0.0.1"
+        "gffpal==v0.0.1"
     ],
 
     dependency_links=[
         # Link to your GitHub repository
-        'https://github.com/KristinaGagalova/gffpal/archive/refs/tags/v0.0.1'
+        'git+ssh://git@github.com/kristinagagalova/gffpal.git@v0.0.1#egg=gffpal-v0.0.1'
     ]
 
     # List additional groups of dependencies here (e.g. development
